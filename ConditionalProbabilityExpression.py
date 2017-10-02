a8bcba191741ece41e83985042224e4bc7927ad3
cmrudi <cutmeurahrudi@gmail.com>
2017-10-02 07:40:35 +0700

    update prob service

--- ConditionalProbabilityExpression.py
+++ ConditionalProbabilityExpression.py
@@ -2,13 +2,43 @@
 class ConditionalProbabilityExpression:
     first = 0
     second = 0
+    third = 0;
 
-    def __init__(self, first, second):
-      self.first = first
-      self.second = second
+    #Define probability as P(first|second)
+    def set_two_param(self, one, two):
+        self.first = str(one);
+        self.second = str(two);
+
+    #Define probability as P(first\second,third)
+    def set_three_param(self, one, two, three):
+        self.first = str(one)
+        self.second = str(two)
+        self.third = str(three);
 
     def get_first(self):
     	return self.first
 
     def get_second(self):
     	return self.second
+
+    def get_third(self):
+        return self.third
+
+    def is_equal(self, key, num_param):
+        if (param == 2):
+            one, two = key.split('|')
+            if (one == self.first and two == self.second):
+                return true
+        elif (param == 4):
+            one , temp = key.split('|')
+            two, third = temp.split(',')
+            if (one == self.first and two == self.second and three == self.third):
+                return true
+        else:
+            return false
+
+    def get_key(self):
+        if (self.third != 0):
+            return self.first + '|' + self.second + ',' + self.third
+        else:
+            return self.first + '|' + self.second
--- TagProbabilityService.py
+++ TagProbabilityService.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from collections import OrderedDict
 from conllu.parser import parse, parse_tree, parse_line
 from Tag import Tag
@@ -7,30 +8,38 @@ import pickle
 import os.path
 
 class TagProbabilityService:
-    #P[tag|word]
     #Represeted as [ConditionalProbabilityExpression , probability value]
     #ConditionalProbabilityExpression is P[tag|word]
-    prob_dict = OrderedDict()
+    tw_prob_dict = OrderedDict()
+    #ConditionalProbabilityExpression is P[word\tag]
+    wt_prob_dict = OrderedDict()
+    #ConditionalProbabilityExpression is P[tag\tag-1,tag-2]
+    #For first word in sentence:
+    #   tag-1 = Tag.NOTEXIST
+    #   tag-2 = Tag.NOTEXIST
+    ttt_prob_dict = OrderedDict()
+
 
     #Represented as [word, count]
     word_dict = OrderedDict()
-
     #Represented as [tag|word, count]
     tag_word_dict = OrderedDict()
+    #Represented as [tag, tag-1, tag-2]
+    ttt_dict = OrderedDict()
+    #Represented as [tag-1, tag-2]
+    tt_dict = OrderedDict()
+    #Represented as [tag]
+    t_dict = OrderedDict()
 
     #File Path to save TagProbabilityService object
-    object_file_path = 'tag_probability_service.dat'
+    tw_object_file_path = 'single_tag_probability_service.dat'
+    wt_object_file_path = 'three_tag_probability_service.dat'
+    ttt_object_file_path = 'three_tag_probability_service.dat'
 
 
     def __init__(self, file_path):
-        if os.path.exists(self.object_file_path):
-            with open(self.object_file_path, "rb") as f:
-                dump = pickle.load(f)
-                self.prob_dict = dump
-                
-                cond = ConditionalProbabilityExpression(self.prob_dict.keys()[0].get_first(),self.prob_dict.keys()[0].get_second())
-                print self.prob_dict[cond]
-
+        if os.path.exists(self.tw_object_file_path) and os.path.exists(self.tw_object_file_path) and os.path.exists(self.ttt_object_file_path):
+            self.read_external_file()
         else :
             corpus_file = open(file_path,'r')
             corpus_data = re.sub(r" +", r"\t",corpus_file.read())
@@ -40,48 +49,122 @@ class TagProbabilityService:
             for sentence in sentence_list:
                 print i
                 i = i + 1
+
+                tag1 = Tag.NOTEXIST
+                tag2 = Tag.NOTEXIST
                 for token in sentence:
 
                     word = token['form']
                     tag_detector = Tag.NOTEXIST
                     tag = tag_detector.get_tag(token['upostag'])
 
-                    expression_tag_word = ConditionalProbabilityExpression(tag,word)
+                    self.update_word_dict(word)
+                    self.update_tag_word_dict(str(tag)+'|'+word)
 
-                    if word in self.word_dict.keys():
-                        value = self.word_dict[word]
-                        value = value + 1
-                        self.word_dict[word] = value
-                    else :
-                        self.word_dict[word] = 1
+                    ttt_tag_key = str(tag) + '|' + str(tag1) + '|' + str(tag2)
+                    tt_tag_key = str(tag1) + '|' + str(tag2)
+                    self.update_ttt_dict(ttt_tag_key)
+                    self.update_tt_dict(tt_tag_key)
+                    self.update_t_dict(str(tag))
 
+                    #update tag
+                    tag2 = tag1
+                    tag1 = tag
 
-                    if expression_tag_word in self.tag_word_dict.keys():
-                        value = self.tag_word_dict[expression_tag_word]
-                        value = value + 1
-                        self.tag_word_dict[expression_tag_word] = value
-                    else :
-                        self.tag_word_dict[expression_tag_word] = 1
-
-            self.count_prob_tag_word()
-            with open(self.object_file_path, "wb") as f:
-                pickle.dump(self.prob_dict, f, pickle.HIGHEST_PROTOCOL)
-
-            for t in self.prob_dict:
-                print t
+            self.count_tw_prob_dict()
+            self.count_wt_prob_dict()
+            self.count_ttt_prob_dict()
+            self.write_external_file()
 
 
-    def count_prob_tag_word(self):
+    def count_tw_prob_dict(self):
         for tag_word in self.tag_word_dict:
-            self.prob_dict[tag_word] = self.tag_word_dict[tag_word]/self.word_dict[tag_word.get_second()]
+            tag, word = tag_word.split('|')
+            self.tw_prob_dict[tag_word] = self.tag_word_dict[tag_word]/self.word_dict[word]
 
-    def get_prob_dict(self):
-        return self.prob_dict
+    def count_wt_prob_dict(self):
+        for tag_word in self.tag_word_dict:
+            tag, word = tag_word.split('|')
+            cond = ConditionalProbabilityExpression()
+            cond.set_two_param(word,tag)
+            self.wt_prob_dict[cond.get_key()] = self.tag_word_dict[tag_word]/self.t_dict[tag]
 
-    def get_tag_word_dict(self):
-        return self.tag_word_dict
+
+    def count_ttt_prob_dict(self):
+        for ttt in self.ttt_dict:
+            tag, tag1, tag2 = ttt.split('|')
+            cond = ConditionalProbabilityExpression()
+            cond.set_three_param(tag, tag1, tag2)
+            self.ttt_prob_dict[cond.get_key()] = self.ttt_dict[ttt]/self.tt_dict[tag1 + '|' + tag2];
+
+    def get_tw_prob_dict(self):
+        return self.tw_prob_dict
+
+    def get_wt_prob_dict(self):
+        return self.wt__probdict
+
+    def get_ttt_prob_dict(self):
+        return self.ttt_prob_dict;
+
+    def update_ttt_dict(self, key):
+        if key in self.ttt_dict:
+            value = self.ttt_dict[key]
+            self.ttt_dict[key] = value + 1
+        else :
+            self.ttt_dict[key] = 1
+
+    def update_tt_dict(self, key):
+        if key in self.tt_dict:
+            value = self.tt_dict[key]
+            self.tt_dict[key] = value + 1
+        else:
+            self.tt_dict[key] = 1
+
+    def update_t_dict(self, key):
+        if key in self.t_dict:
+            value = self.t_dict[key]
+            self.t_dict[key] = value + 1
+        else:
+            self.t_dict[key] = 1
+
+    def update_word_dict(self, word):
+        if word in self.word_dict.keys():
+            value = self.word_dict[word]
+            value = value + 1
+            self.word_dict[word] = value
+        else :
+            self.word_dict[word] = 1
+
+    def update_tag_word_dict(self, key):
+        if key in self.tag_word_dict.keys():
+            value = self.tag_word_dict[key]
+            value = value + 1
+            self.tag_word_dict[key] = value
+        else :
+            self.tag_word_dict[key] = 1
+
+    def read_external_file(self):
+        with open(self.tw_object_file_path, "rb") as f:
+            dump = pickle.load(f)
+            self.tw_prob_dict = dump
+
+        with open(self.wt_object_file_path, "rb") as f:
+            dump = pickle.load(f)
+            self.wt_prob_dict = dump
+
+        with open(self.ttt_object_file_path, "rb") as f:
+            dump = pickle.load(f)
+            self.ttt_prob_dict = dump
+
+    def write_external_file(self):
+        with open(self.tw_object_file_path, "wb") as f:
+            pickle.dump(self.tw_prob_dict, f, pickle.HIGHEST_PROTOCOL)
+        with open(self.wt_object_file_path, "wb") as f:
+            pickle.dump(self.wt_prob_dict, f, pickle.HIGHEST_PROTOCOL)
+        with open(self.ttt_object_file_path, "wb") as f:
+            pickle.dump(self.ttt_prob_dict, f, pickle.HIGHEST_PROTOCOL)
 
 tag = TagProbabilityService("UD_English/en-ud-test.conllu")
-print '==========='
-#for t in tag.get_prob_dict():
-    #print t.get_second()
+prob_dict = tag.get_tw_prob_dict()
+#P(Tag.ADJ|rice)
+print prob_dict[str(Tag.NOUN) + '|' + 'meeting']
